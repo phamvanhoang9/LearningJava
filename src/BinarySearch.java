a58b5db33f124b21e70a6140a567c732ddf3e685
Steve Hoang <hoang.pv192875@sis.hust.edu.vn>
2024-04-29 17:37:31 +0700

    update

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class BinarySearch {
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 5, 6, 7, 8, 9};
+        System.out.println(binarySearch(array, 7));
+    }
 }
